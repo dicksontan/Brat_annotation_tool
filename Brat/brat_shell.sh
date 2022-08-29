aa397326d9560b64a942d8d279eb8cb5abfab9b3
dicksontan <50508538+dicksontan@users.noreply.github.com>
2022-08-29 17:39:32 +0800

    commit files

---
+++ Brat/brat_shell.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo 'Creating Config File...'
+
+echo > ./desktop/brat_annotations/annotation.conf
+
+echo 'Creating Entity Files...'
+
+python ./Desktop/Vs_Code_Projects/brat/brat_run.py
+
+echo 'Transferring Entity Files to Linux VM containing volumes...'
+
+chmod -R u=rwx,g=rwx,o=rwx ./desktop/brat_annotations
+
+docker cp ./desktop/brat_annotations c5a09d229264:bratdata/examples/mt_samples
+
+echo 'Entity Files Transferred'
